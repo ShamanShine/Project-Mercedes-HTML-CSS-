e26a10031ab1423033a6314ccc2926e75af373da
ShamanShine <lazun2016@gmail.com>
2024-04-04 21:32:12 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ $(function () {
     slidesToShow: 4,
     variableWidth: true,
     prevArrow:
-      '<img class="arrow arrow-left" src="./images/arrow-left.svg" alt="arrow-left">',
+      '<img class="arrow arrow-left" src="../images/arrow-left.svg" alt="arrow-left">',
     nextArrow:
-      '<img class="arrow arrow-right" src="./images/arrow-right.svg" alt="arrow-right">',
+      '<img class="arrow arrow-right" src="../images/arrow-right.svg" alt="arrow-right">',
   });
 });
